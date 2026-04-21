3bd5a9f4a587442e7b6b36546d282d9b524569e3
Your Name <joshuanakalelo.mail@gmail.com>
2026-04-21 12:44:35 +0700

    first commit

---
+++ app/Models/Store.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use UUID;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'logo',
+        'about',
+        'phone',
+        'address_id',
+        'city',
+        'address',
+        'postal_code',
+        'is_verified'
+
+    ];
+
+    //relationship one store has one user
+    public function user () {
+        return $this->belongsTo(User::class);
+    }  
+    
+    public function storeBallance() {
+        return $this->hasOne(StoreBallance::class);
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\UUID;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
+class User extends Authenticatable
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable, UUID;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+    
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        
+    ];
+    
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    //User can have one store relationship
+    public function store() {
+        return $this->hasOne(Store::class);
+    }
+}
