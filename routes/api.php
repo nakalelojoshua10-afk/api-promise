cd6b509001182657f142e7d69b7266a5c9cf17c5
Your Name <joshuanakalelo.mail@gmail.com>
2026-05-11 19:59:30 +0700

    GetAll Data 3 Material (edited)

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('user', UserController::class);
-Route::get('user/all/paginaterd', [UserController::class, 'getAllPaginated']);
+Route::get('user/all/paginated', [UserController::class, 'getAllPaginated']);
