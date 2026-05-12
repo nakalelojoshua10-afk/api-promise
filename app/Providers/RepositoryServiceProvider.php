86e2146f69dfa850d39992df3580dd2493e3f114
Your Name <joshuanakalelo.mail@gmail.com>
2026-05-12 22:11:32 +0700

    Get All Data 2 (API STORE)

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Interfaces\StoreRepositoryInterface;
+use App\Interfaces\UserRepositoryInterface;
+use App\Repositories\StoreRepository;
+use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(StoreRepositoryInterface::class, StoreRepository::class);
     }
 
     /**
