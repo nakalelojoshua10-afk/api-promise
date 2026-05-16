946d3d5b311cf674c597888a966b9949c5418ed9
Your Name <joshuanakalelo.mail@gmail.com>
2026-05-17 01:25:10 +0700

    Create Data (API Store Balance History)

--- app/Interfaces/StoreBalanceHistoryRepositoryInterface.php
+++ app/Interfaces/StoreBalanceHistoryRepositoryInterface.php
@@ -8,4 +8,5 @@ interface StoreBalanceHistoryRepositoryInterface
     public function getAll(?string $search, ?int $limit, bool $execute);
     public function getAllPaginated(?string $search, ?int $rowPerPage);
     public function getById(?string $id);
+    public function create(array $data);
 }
--- app/Repositories/StoreBalanceHistoryRepository.php
+++ app/Repositories/StoreBalanceHistoryRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories; // 👈 FIXED: Changed from App\Interfaces to App\Re
 
 use App\Interfaces\StoreBalanceHistoryRepositoryInterface; // 👈 IMPORTED the interface
 use App\Models\StoreBalanceHistory;
+use Illuminate\Support\Facades\DB;
 
 class StoreBalanceHistoryRepository implements StoreBalanceHistoryRepositoryInterface
 {
@@ -40,5 +41,28 @@ class StoreBalanceHistoryRepository implements StoreBalanceHistoryRepositoryInte
     public function getById(?string $id)
     {
         return StoreBalanceHistory::find($id);
+    }
+
+    public function create(array $data)
+    {
+        DB::beginTransaction();
+
+        try {
+            $storeBalanceHistory = StoreBalanceHistory::create([
+                'store_balance_id' => $data['store_balance_id'],
+                'type' => $data['type'],
+                'reference_id' => $data['reference_id'], 
+                'reference_type' => $data['reference_type'],
+                'amount' => $data['amount'],
+                'remaks' => $data['remarks'],
+            ]);
+
+            DB::commit();
+            return $storeBalanceHistory;
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw new \Exception("Repository Error: " . $e->getMessage());
+        }
     }
 }
